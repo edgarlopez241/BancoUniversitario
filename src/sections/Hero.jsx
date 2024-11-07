2274ffb27129ad433b2df47eb065764c5ff8278c
edgarlopez241 <edgarlopez241@gmail.com>
2024-11-07 11:54:59 -0400

    Reparado los errores de la parte responsive

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
             <div className="caption small-2 uppercase text-p3">
               Banco Universitario
             </div>
-            <h1 className="mb-6 h-1 text-p4 uppercase max-lg:mb-7 max-lg:h-2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+            <h1 className="mb-6 h-1 text-p4 uppercase max-lg:mb-7 max-lg:h-2 max-md:mb-4 max-md:text-5xl max-md:leading-12 hidden sm:block">
               Banca Universitaria al alcanze de tus manos
             </h1>
-            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10 ">
               5 años contigo, El banco Universitario te ofrece soluciones
               financieras personalizadas para estudiantes. Unete a nuestra
               comunidad
